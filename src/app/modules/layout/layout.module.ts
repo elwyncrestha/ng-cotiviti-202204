827ff392b39e493086f5429bc79edfe40ec40f2e
Elvin Shrestha <elwyncrestha@gmail.com>
2022-04-22 10:35:32 +0545

    Day 3: Using routerLinkActive

--- src/app/modules/layout/layout.module.ts
+++ src/app/modules/layout/layout.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -12,6 +13,7 @@ import { HeaderComponent } from './components/header/header.component';
   ],
   imports: [
     CommonModule,
+    RouterModule
   ],
   exports: [
     HeaderComponent,
